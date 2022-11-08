18c12cefb03593a3b29713bdd9c2e635ea718d36
jaxzzxaj <programyourlife1020@gmail.com>
2022-11-08 11:58:57 +0900

    各columnの値の正規表現を指定＋エラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,13 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   VALID_KANJI_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/
   VALID_RUBY_REGEX = /\A[ァ-ヶ]+\z/
+
+
+
+  validates_format_of :email, with: VALID_EMAIL_REGEX, message: 'には「@」を含めてください'
+  validates_format_of :password, with: VALID_PASSWPRD_REGEX, message: 'には英語と数字の両方で入力してください'
+  validates_format_of :first_name_kanji, with: VALID_KANJI_REGEX, message: 'には全角カタカナ、ひらがな、漢字で入力してください'
+  validates_format_of :last_name_kanji, with: VALID_KANJI_REGEX, message: 'には全角カタカナ、ひらがな、漢字で入力してください'
+  validates_format_of :first_name_ruby, with: VALID_RUBY_REGEX, message: 'には全角カタカナのみで入力してください'
+  validates_format_of :last_name_ruby, with: VALID_RUBY_REGEX, message: 'には全角カタカナのみで入力してください'
 end
