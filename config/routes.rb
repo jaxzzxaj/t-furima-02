fa9cb3a9c5caaf4ecf830d53ee9fc5c5a0498efa
jaxzzxaj <programyourlife1020@gmail.com>
2022-11-08 10:50:20 +0900

    コメントアウト
    rails g devise userをしたあとに自動で入力される"devise_for :users"について

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #スコープをしている
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #デフォルト→  devise_for{:users path: 'users' method: 'devise'}
 end
