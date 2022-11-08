a62b7e1eefc8541c56c1969c7729be30c4bdee12
jaxzzxaj <programyourlife1020@gmail.com>
2022-11-08 12:15:41 +0900

    新規登録時に取得するパラメーターを指定。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :first_name_kanji, :first_name_ruby, :last_name_kanji, :last_name_ruby, :date_of_birth]
+  end
 end
