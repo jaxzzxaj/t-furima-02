0cf5324a7f31b7d779fcf2a8da35280b4d12d7ee
jaxzzxaj <programyourlife1020@gmail.com>
2022-11-08 12:17:55 +0900

    コメントアウト
    箱の処理内容の説明

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  #devise_parameter_sanitizer＝値を取得してくれるメソッド。.permit（どの時？,keys:[:どの値？])
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :first_name_kanji, :first_name_ruby, :last_name_kanji, :last_name_ruby, :date_of_birth]
   end
