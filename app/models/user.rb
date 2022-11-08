0ec1fdbbf66eac6656a89382b7ddfc02900ed8ef
jaxzzxaj <programyourlife1020@gmail.com>
2022-11-08 11:28:46 +0900

    コメントアウト
    "rails g devise user"を打ってuser.rbで自動生成されるものの説明

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  #database_authenticatable＝パスワードを暗号化してくれる
+  #registerable＝登録処理でuserをサインアップしてくれる＋アカウントを編集・削除できる
+  #recoverable=パスワードをリセットして通知してくれる
+  #rememberable=userを記憶するためのトークンを生成・削除してくれる
+  #validatable=メールやパスワードのバリデーションを提供してくれる
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/migrate/20221108021247_devise_create_users.rb
+++ db/migrate/20221108021247_devise_create_users.rb
@@ -4,6 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,              null: false
+      t.string :first_name_kanji,              null: false
+      t.string :first_name_ruby,              null: false
+      t.string :last_name_kanji,              null: false
+      t.string :last_name_ruby,              null: false
+      t.string :date_of_birth,              null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
