e9f2110abaaa2df4d9c799187e6031bb5c0abd79
jaxzzxaj <programyourlife1020@gmail.com>
2022-11-09 18:20:09 +0900

    エラー発生
    ＜user.rb:32:in `<class:User>': uninitialized constant User (call 'User.connection' to establish a connection)::VALID_PASSWPRD_REGEX (NameError)と怒られた
    
    直前の行動：NoMethodErrorの解決→関係なし
    
    ーーーー
    （解決）
    NameErrorと怒られているので名前がおかしいとのこと。ターミナルに書いてあるとおり、スペルミスだった。
    
    rails sで動作確認後、解決した

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
 
   #箱を使って各columnの記入の仕方を指定し、エラーが発生したときはcolumn+メッセージを出すよう設定
   validates_format_of :email, with: VALID_EMAIL_REGEX, message: 'には「@」を含めてください'
-  validates_format_of :password, with: VALID_PASSWPRD_REGEX, message: 'には英語と数字の両方で入力してください'
+  validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'には英語と数字の両方で入力してください'
   validates_format_of :first_name_kanji, with: VALID_KANJI_REGEX, message: 'には全角カタカナ、ひらがな、漢字で入力してください'
   validates_format_of :last_name_kanji, with: VALID_KANJI_REGEX, message: 'には全角カタカナ、ひらがな、漢字で入力してください'
   validates_format_of :first_name_ruby, with: VALID_RUBY_REGEX, message: 'には全角カタカナのみで入力してください'
