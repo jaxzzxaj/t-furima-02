1957997a24aa9af3957d1c78e65d060364a9e4f1
jaxzzxaj <programyourlife1020@gmail.com>
2022-11-12 10:32:04 +0900

    エラー修正
    application_controller.rb:19: syntax error, unexpected end, expecting ')'
    
    ) が抜けてた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   #条件：devise関連の画面へ遷移したとき。処理内容:configure_permitted_parametersをしてください
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-
+  
   #devise_parameter_sanitizer＝値を取得してくれるメソッド。.permit（どの時？,keys:[:どの値？])
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :first_name_kanji, :first_name_ruby, :last_name_kanji, :last_name_ruby, :date_of_birth]
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :first_name_kanji, :first_name_ruby, :last_name_kanji, :last_name_ruby, :date_of_birth])
   end
 end
