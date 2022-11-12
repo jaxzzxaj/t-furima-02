446a25e57e760165e722ea981c6ef51e47c7d590
jaxzzxaj <programyourlife1020@gmail.com>
2022-11-12 21:40:40 +0900

    indexへ遷移できるよう設定する
    (routes)
    root to:で"/"の遷移先を指定
    
    ※rails routesで遷移先が見れるよ
    resourcesでなんのテーブルのアクションを動かすか設定
    
    --
    (controller)
    controllerで各アクションの箱を作る
    
    --
    (views)
    試しにつなぐかどうか"helloooo"と打ってみた
    
    上記完了後、"rails s "確認してみた

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   #スコープをしている
   devise_for :users
   #デフォルト→  devise_for{:users path: 'users' method: 'devise'}
+
+  root to: "product#index"
+  resources :products, only:[:index, :new, :create]
 end
